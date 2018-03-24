aa0aab40ba55702d6b9fcb63392ba56dc1006df7
anonewb <anonewbe@gmail.com>
2018-03-24 10:11:29 +0530

    Deferring App install banner

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -1,3 +1,5 @@
+var deferredPrompt;
+
 if('serviceWorker' in navigator){ // check if serviceWorker prop is present in navigator obj
     navigator.serviceWorker
         .register('/sw.js') // register this file as SW
@@ -5,3 +7,10 @@ if('serviceWorker' in navigator){ // check if serviceWorker prop is present in n
             console.log('Service worker registered');
         });
 }
+
+window.addEventListener('beforeinstallprompt', function(event){
+    console.log('beforeinstallprompt fired');
+    event.preventDefault();
+    deferredPrompt = event;
+    return false;
+});
