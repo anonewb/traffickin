d363005fdecc093a790313b9a2094a49f9cf0396
anonewb <anonewbe@gmail.com>
2018-03-24 09:40:00 +0530

    Non-Lifecycle events

--- public/sw.js
+++ public/sw.js
@@ -2,7 +2,13 @@ self.addEventListener('install', function(event){
     console.log('[Service Worker] Installing SW...', event);
 });
 
-self.addEventListener('activate', function(event){
+self.addEventListener('activate', function(event){ 
     console.log('[Service Worker] Activating SW...', event);
     return self.clients.claim();
 });
+// install and active events are triggered by browser
+
+self.addEventListener('fetch', function(event){ //fetch event is triggered by webApp
+    console.log('[Service Worker] Fetching something...', event);
+    event.respondWith(fetch(event.request));
+}); 
