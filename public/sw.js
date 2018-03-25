09e0ad880ea99863e79762b9fc05c2898539fa96
anonewb <anonewbe@gmail.com>
2018-03-25 18:30:51 +0530

    ***Adding and retrieving multiple files (to from cache)

--- public/sw.js
+++ public/sw.js
@@ -4,6 +4,8 @@ self.addEventListener('install', function(event){
     caches.open('static') //static will be the name of the cache
       .then(function(cache){
         console.log('[Service Worker] Precaching App Shell');
+        cache.add('/'); // think about this as requests not as paths
+        cache.add('/index.html'); // you have urls here, caching urls i.e caching requests
         cache.add('/src/js/app.js');
       })
   )
