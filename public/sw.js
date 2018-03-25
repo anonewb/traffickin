34e7e1e772311d9b059aad5d14f47d40e1eef408
anonewb <anonewbe@gmail.com>
2018-03-25 18:50:25 +0530

    Cache Multiple Files with addAll

--- public/sw.js
+++ public/sw.js
@@ -4,9 +4,24 @@ self.addEventListener('install', function(event){
     caches.open('static') //static will be the name of the cache
       .then(function(cache){
         console.log('[Service Worker] Precaching App Shell');
-        cache.add('/'); // think about this as requests not as paths
-        cache.add('/index.html'); // you have urls here, caching urls i.e caching requests
-        cache.add('/src/js/app.js');
+        // cache.add('/'); // think about this as requests not as paths
+        // cache.add('/index.html'); // you have urls here, caching urls i.e caching requests
+        // cache.add('/src/js/app.js');
+        cache.addAll([
+          '/',
+          '/index.html',
+          '/src/js/app.js',
+          '/src/js/feed.js',
+          '/src/js/promise.js',
+          '/src/js/fetch.js',
+          '/src/js/material.min.js',
+          '/src/css/app.css',
+          '/src/css/feed.css',
+          '/src/images/main-image.jpg',
+          'https://fonts.googleapis.com/css?family=Roboto:400,700',
+          'https://fonts.googleapis.com/icon?family=Material+Icons',
+          'https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.3.0/material.indigo-pink.min.css'
+        ]);
       })
   )
 });
