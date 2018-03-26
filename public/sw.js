4eead9570591ec71aec56934fcd88a1753bfe5ee
anonewb <anonewbe@gmail.com>
2018-03-26 07:19:36 +0530

    err handling & cache versioning and management

--- public/sw.js
+++ public/sw.js
@@ -1,7 +1,11 @@
-self.addEventListener('install', function(event){
+
+var CACHE_STATIC_NAME = 'static-v4';
+var CACHE_DYNAMIC_NAME = 'dynamic-v2';
+
+self.addEventListener('install', function(event){ //***this install stage will be triggered when user visit's pg for first time
   console.log('[Service Worker] Installing SW...', event);
   event.waitUntil(
-    caches.open('static') //static will be the name of the cache
+    caches.open(CACHE_STATIC_NAME) //CACHE_STATIC_NAME will be the name of the cache. GOTO Application>cache storage
       .then(function(cache){
         console.log('[Service Worker] Precaching App Shell');
         // cache.add('/'); // think about this as requests not as paths
@@ -26,8 +30,19 @@ self.addEventListener('install', function(event){
   )
 });
 
-self.addEventListener('activate', function(event){ 
+self.addEventListener('activate', function(event){ //***this activate stage will be triggered after user's first visit
   console.log('[Service Worker] Activating SW...', event);
+  event.waitUntil(
+    caches.keys()
+      .then(function(keyList) { //keyList contains names of all cache that are stored
+        return Promise.all(keyList.map(function(key) {
+          if (key !== CACHE_STATIC_NAME && key !== CACHE_DYNAMIC_NAME) {
+            console.log('[Service Worker] Removing old cache.', key);
+            return caches.delete(key); //deleting old cache file if sw.js is changed
+          }
+        }));
+      })
+  );
   return self.clients.claim();
 });
 // install and active events are triggered by browser
@@ -42,12 +57,15 @@ self.addEventListener('fetch', function(event){ //fetch event is triggered by we
         else {
           return fetch(event.request)
             .then(function(res){
-              return caches.open('dynamic')
+              return caches.open(CACHE_DYNAMIC_NAME)
                 .then(function(cache){
                   cache.put(event.request.url, res.clone());
                   return res;
                 })
-            });      
+            })
+            .catch(function(err){
+
+            });
         }
       })
   );
