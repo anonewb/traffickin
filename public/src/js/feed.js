de89a4221b41123071272e5ab1039e495f110a0d
anonewb <anonewbe@gmail.com>
2018-03-26 19:41:37 +0530

    Adv caching: [Removed] offering cache on demand

--- public/src/js/feed.js
+++ public/src/js/feed.js
@@ -30,17 +30,17 @@ shareImageButton.addEventListener('click', openCreatePostModal);
 
 closeCreatePostModalButton.addEventListener('click', closeCreatePostModal);
 
-// Allows to save assets in cache on demand of user to save article
-function onSaveButtonClicked(event) {
-  console.log('clicked');
-  if ('caches' in window) {
-    caches.open('user-requested')
-      .then(function(cache){
-        cache.add('https://httpbin.org/get');
-        cache.add('/src/images/sf-boat.jpg');
-      })
-  }
-}
+// // Allows to save assets in cache on demand of user to save article
+// function onSaveButtonClicked(event) {
+//   console.log('clicked');
+//   if ('caches' in window) {
+//     caches.open('user-requested')
+//       .then(function(cache){
+//         cache.add('https://httpbin.org/get');
+//         cache.add('/src/images/sf-boat.jpg');
+//       })
+//   }
+// }
 
 function createCard() {
   var cardWrapper = document.createElement('div');
@@ -60,10 +60,10 @@ function createCard() {
   cardSupportingText.className = 'mdl-card__supporting-text';
   cardSupportingText.textContent = 'In San Francisco';
   cardSupportingText.style.textAlign = 'center';
-  var cardSaveButton = document.createElement('button');
-  cardSaveButton.textContent = 'Save';
-  cardSaveButton.addEventListener('click', onSaveButtonClicked);
-  cardSupportingText.appendChild(cardSaveButton);
+  // var cardSaveButton = document.createElement('button');
+  // cardSaveButton.textContent = 'Save';
+  // cardSaveButton.addEventListener('click', onSaveButtonClicked);
+  // cardSupportingText.appendChild(cardSaveButton);
   cardWrapper.appendChild(cardSupportingText);
   componentHandler.upgradeElement(cardWrapper);
   sharedMomentsArea.appendChild(cardWrapper);
