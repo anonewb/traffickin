88fa11c6043b7c193d08ed39ab4145ab9182c67b
anonewb <anonewbe@gmail.com>
2018-04-01 10:29:26 +0530

    Web Push Notifications: Reacting to Notification Interaction - Clicks & Closing [ez]

--- public/sw.js
+++ public/sw.js
@@ -230,3 +230,22 @@ self.addEventListener('sync', function(event) { //whenever connectivity is re-es
     );
   }
 });
+
+self.addEventListener('notificationclick', function(event) {
+  var notification = event.notification;
+  var action = event.action;
+
+  console.log(notification);
+
+  if (action === 'confirm') {
+    console.log('Confirm was chosen');
+    notification.close();
+  } else {
+    console.log(action);
+    notification.close();
+  }
+});
+
+self.addEventListener('notificationclose', function(event) {
+  console.log('Notification was closed', event);
+});
