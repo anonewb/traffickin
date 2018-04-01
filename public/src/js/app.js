2f71afee4137b73028f9f2c1ef0e09c404df6caa
anonewb <anonewbe@gmail.com>
2018-04-01 10:47:13 +0530

    Web Push Notifications: Creating a push subscription

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -46,6 +46,23 @@ function displayConfirmNotification() {
   }
 }
 
+function configurePushSub() {
+  if (!('serviceWorker' in navigator)) {
+    return;
+  }
+
+  navigator.serviceWorker.ready
+    .then(function(swreg) {
+      return swreg.pushManager.getSubscription();
+    })
+    .then(function(sub) {
+      if (sub === null) {
+        // Create a new subscription
+      } else {
+        // We have a subscription
+      }
+    });
+}
 
 function askForNotificationPermission() {
     Notification.requestPermission(function(result) {
@@ -53,12 +70,13 @@ function askForNotificationPermission() {
       if (result !== 'granted') {
         console.log('No notification permission granted!');
       } else {
-        displayConfirmNotification();
+        configurePushSub();
+        // displayConfirmNotification();
       }
     });
   }
 
-if ('Notification' in window) {
+if ('Notification' in window && 'serviceWorker' in navigator) {
     for (var i = 0; i < enableNotificationsButtons.length; i++) {
       enableNotificationsButtons[i].style.display = 'inline-block';
       enableNotificationsButtons[i].addEventListener('click', askForNotificationPermission);
