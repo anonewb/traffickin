dbf9df99d07199c5d7b5f1bea7d985b6ab0b7e7b
anonewb <anonewbe@gmail.com>
2018-04-01 15:42:07 +0530

    Web Push Notifications: **Sending push notifications from server

--- functions/index.js
+++ functions/index.js
@@ -1,6 +1,9 @@
+// this is the nodejs code running in firebase server
+
 var functions = require('firebase-functions');
 var admin = require('firebase-admin'); //access to firebase db
 var cors = require('cors')({origin: true}); //gives cross origin access
+var webpush = require('web-push');
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
@@ -15,13 +18,31 @@ admin.initializeApp({
 
 exports.storePostData = functions.https.onRequest(function(request, response) {
     cors(request, response, function() { //accessing req n res from parent onRequest()
-      admin.database().ref('posts').push({
+      admin.database().ref('posts').push({ //core part of server where db is stored
         id: request.body.id,
         title: request.body.title,
         location: request.body.location,
         image: request.body.image
       })
-        .then(function() {
+        .then(function() { //this will run only if data is stored successfully
+          webpush.setVapidDetails('mailto:business@ano.com', 'BKbNXqUJ0PuBQdkdinhIw1r7kalw3Lfc_hpng72lMxvcFbAmUNFNsTbadyCyxcmCh3XStur7Sd26OgMJapvpdYc', 'KI-RuBsrAcQPlqfcPwfLUPMI_hCrOUckhjNQVdaTFak');
+          return admin.database().ref('subscriptions').once('value');
+        })
+        .then(function (subscriptions) {
+          subscriptions.forEach(function (sub) { //sub holds endpoint and keys
+            var pushConfig = {
+              endpoint: sub.val().endpoint,
+              keys: {
+                auth: sub.val().keys.auth,
+                p256dh: sub.val().keys.p256dh
+              }
+            };
+  
+            webpush.sendNotification(pushConfig, JSON.stringify({title: 'New Post', content: 'New Post added!'})) //2nd arg of sendNotification() is paypload
+              .catch(function(err) {
+                console.log(err);
+              })
+          });
           response.status(201).json({message: 'Data stored', id: request.body.id});
         })
         .catch(function(err) {
