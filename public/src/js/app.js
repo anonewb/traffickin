e34dbf27370fcbe18a4853bf72bff078a16f7cc9
anonewb <anonewbe@gmail.com>
2018-08-05 16:17:38 +0530

    sw path

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -13,7 +13,7 @@ if (!window.Promise) {
 if ('serviceWorker' in navigator) {
 	// Feature Detection- check if serviceWorker prop is present in navigator obj
 	navigator.serviceWorker
-		.register('/sw.js') // register this file as SW
+		.register('../../sw.js') // register this file as SW
 		.then(function() {
 			console.log('Service worker registered');
 		})
